ba838e9046900058153ac5052f1231e441414562
giuszeppe <giuseppe@mwspace.com>
2023-03-14 19:03:48 +0100

    added configuration for authentication libraries

--- config/config.exs
+++ config/config.exs
@@ -47,6 +47,15 @@ config :logger, :console,
 # Use Jason for JSON parsing in Phoenix
 config :phoenix, :json_library, Jason
 
+# Ueberauth configs for spotify
+config :ueberauth, Ueberauth,
+  providers: [
+    spotify: {Ueberauth.Strategy.Spotify, [default_scope: "playlist-read-private"]}
+  ]
+config :ueberauth, Ueberauth.Strategy.Patreon.OAuth,
+  client_id: System.get_env("SPOTIFY_CLIENT_ID"),
+  client_secret: System.get_env("SPOTIFY_CLIENT_SECRET")
+
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{config_env()}.exs"
---
+++ config/dev.secret.exs
@@ -0,0 +1,5 @@
+import Config
+
+config :ueberauth, Ueberauth.Strategy.Patreon.OAuth,
+  client_id: "897ab0d9d6f6400c81f049a301c071d0",
+  client_secret: "cfa6f807a6364abe90b208a0e8ef0730"
