c89b4552cbf1f2423d2bc7e33272149824037bbd
giuszeppe <giuseppe@mwspace.com>
2023-03-14 19:05:48 +0100

    added oauth strategy for spotify

---
+++ lib/ueberauth/strategy/spotify.ex
@@ -0,0 +1,57 @@
+defmodule Ueberauth.Strategy.Spotify do
+  use Ueberauth.Strategy,
+    oauth2_module: Ueberauth.Strategy.Spotify.OAuth
+
+  alias Ueberauth.Auth.Credentials
+
+  def handle_request!(conn) do
+    params =
+      []
+      |> with_state_param(conn)
+     # Will invoke the OAuth Patreon module we will define next
+
+    module = option(conn, :oauth2_module)
+     # Performs the redirect to Patreon to REQUEST access
+    redirect!(conn,apply(module,:authorize_url!, [params]))
+  end
+
+  def handle_callback!(%Plug.Conn{params: %{"code" => code}} = conn) do
+    module = option(conn, :oauth2_module)
+    # Uses our oauth module to perform the token fetch
+    token = apply(module, :get_token!, [[code: code]])
+
+    if token.access_token == nil do
+      set_errors!(conn, [
+        error(token.other_params["error"], token.other_params["error_description"])
+      ])
+    else
+      conn
+      |> put_private( :spotify_token, token)
+    end
+  end
+
+  def handle_callback!(conn) do
+    set_errors!(conn, [error("missing_code", "No code received")])
+  end
+
+  def handle_cleanup!(conn) do
+    conn
+    |> put_private(:spotify_token, nil)
+  end
+
+
+  def credentials(conn) do
+    token = conn.private.spotify_token
+
+    %Credentials{
+      token: token.access_token,
+      token_type: token.token_type,
+      refresh_token: token.refresh_token,
+      expires_at: token.expires_at
+    }
+  end
+
+  defp option(conn, key) do
+    Keyword.get(options(conn), key, Keyword.get(default_options(), key))
+  end
+end
---
+++ lib/ueberauth/strategy/spotify/oauth.ex
@@ -0,0 +1,75 @@
+defmodule Ueberauth.Strategy.Spotify.OAuth do
+  use OAuth2.Strategy
+
+  @defaults [
+    strategy: __MODULE__,
+    site: "https://open.spotify.com/",
+    authorize_url: "https://accounts.spotify.com/authorize",
+    token_url: "https://accounts.spotify.com/api/token",
+    redirect_uri: "http://localhost:4000/auth/spotify/callback",
+    token_method: :post,
+    scope: "playlist-read-private"
+  ]
+
+  def client(opts \\ []) do
+    # This is where we grab the CLient ID and Client Secret we created earilier
+    config =
+      :ueberauth
+      |> Application.fetch_env!(Ueberauth.Strategy.Patreon.OAuth)
+      |> check_config_key_exists(:client_id)
+      |> check_config_key_exists(:client_secret)
+
+    client_opts =
+      @defaults
+      |> Keyword.merge(config)
+      |> Keyword.merge(opts)
+
+    json_library = Ueberauth.json_library()
+
+    OAuth2.Client.new(client_opts)
+    |> OAuth2.Client.put_serializer("application/json", json_library)
+    |> OAuth2.Client.put_serializer("application/vnd.api+json", json_library)
+  end
+
+  def authorize_url!(params \\ [], opts \\ []) do
+    opts
+    |> client
+    |> OAuth2.Client.authorize_url!(params)
+
+  end
+
+  def authorize_url(client,params) do
+    OAuth2.Strategy.AuthCode.authorize_url(client,params)
+  end
+
+  defp check_config_key_exists(config,key) when is_list(config) do
+    unless Keyword.has_key?(config,key) do
+      raise "#{inspect(key)} missing from config :ueberauth, Ueberauth.Strategy.Spotify"
+    end
+
+    config
+  end
+
+  defp check_config_key_exists(_, _) do
+    raise "Config :ueberauth, Ueberauth.Strategy.Patreon is not a keyword list, as expected"
+  end
+
+  def get_token!(params \\ [], options \\ []) do
+    headers = Keyword.get(options,:headers,[])
+    options = Keyword.get(options,:options,[])
+
+    client_options = Keyword.get(options,:client_options, [])
+    client = OAuth2.Client.get_token!(client(client_options), params, headers, options)
+    client.token
+
+  end
+
+  def get_token(client, params, headers) do
+    client = client
+    |> put_param("grant_type", "authorization_code")
+    |> put_header("Accept", "application/json")
+
+    OAuth2.Strategy.AuthCode.get_token(client, params, headers)
+  end
+
+end
